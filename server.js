49f8b0f74a8026db59baf245fb65281b1526c340
chohs0605 <58533104+chohs0605@users.noreply.github.com>
2019-12-05 14:43:24 -0800

    Upload file secontime

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
+const redirectToHTTPS = require('express-http-to-https').redirectToHTTPS;
 const express = require("express");
 const fetch = require("node-fetch");
 const app = express();
+
+app.use(redirectToHTTPS([/localhost:(\d{4})/], [], 301));
+  
+  //loggoing for each request
+  app.use((req, resp, next) => {
+    const now = new Date();
+    const time = `${now.toLocaleDateString()} - ${now.toLocaleTimeString()}`;
+    const path = `"${req.method} ${req.path}"`;
+    const m = `${req.ip} - ${time} - ${path}`;
+    //eslint-disable-next-line no-console
+    console.log(m);
+    next();
+  });
+
 app.use(express.static("public"));
 
  const listener = app.listen(process.env.PORT, function() {
